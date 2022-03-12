4be9382fbaafdc1742725efb8443f7be95fad9cf
sajal gupta <sajal gupta@LAPTOP-RUQO4NJ4>
2022-03-12 12:04:46 +0530

    new file added

---
+++ LearningJava/src/Unit_01/P2_Task01_KeywordsInJava.java
@@ -0,0 +1,23 @@
+package Unit_01;
+
+public class P2_Task01_KeywordsInJava {
+
+	public static void main(String[] args) {
+		/*
+		 * Cannot used as token
+		 Here we cannot use token(double ) as variable name 
+		 
+		int double=3;
+        */
+	     /*
+	     Here we cannot use this token(int) as function name
+		 void int()
+		 {
+			 System.out.println("Hello");
+		 }
+		 */
+		
+		
+	}
+
+}
