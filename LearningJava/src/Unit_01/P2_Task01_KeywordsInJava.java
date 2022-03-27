db33c5edbcafd2078efc55b44f5c0872cdb80bc1
sajal gupta <sajal gupta@LAPTOP-RUQO4NJ4.mshome.net>
2022-03-27 23:55:00 +0530

    new file added about keyword

--- LearningJava/src/Unit_01/P2_Task01_KeywordsInJava.java
+++ LearningJava/src/Unit_01/P2_Task01_KeywordsInJava.java
@@ -2,13 +2,28 @@ package Unit_01;
 
 public class P2_Task01_KeywordsInJava {
 
-	public static void main(String[] args) {
+	 static int add(int a,int b)
+     {
+    	 System.out.println("In Function static");
+    	 return (a+b);
+     }
+	  int multi(int a, int b)
+	  {
+		  System.out.println("In Function non-static");
+	    	 return (a*b);
+	  }
+	public static void main(String[] args) 
+	{
+		System.out.println("P2_Task01_KeywordsInJava");
 		/*
 		 * Cannot used as token
 		 Here we cannot use token(double ) as variable name 
 		 
 		int double=3;
         */
+		int a=3;
+		int b=4;
+		System.out.println(a);
 	     /*
 	     Here we cannot use this token(int) as function name
 		 void int()
@@ -16,8 +31,18 @@ public class P2_Task01_KeywordsInJava {
 			 System.out.println("Hello");
 		 }
 		 */
-		
-		
+         
+		int c=add(a,b);		
+		System.out.println(c);
+		 /* 
+		   Here we cannot use this token(int) as class  name
+		   
+		   ABC int =new ABC();
+		 */
+		P2_Task01_KeywordsInJava  obj = new P2_Task01_KeywordsInJava();
+		System.out.println(obj.multi(a, b));
 	}
-
+	
+	   
+	
 }
